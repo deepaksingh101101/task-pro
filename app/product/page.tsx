1c737ee1eaa12a92b8a19733df404b13a77e27df
Deepak Singh <deepaksingh101101@gmail.com>
2024-09-17 15:00:47 +0530

    removed subscription

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,5 @@
 import BreadCrumb from '@/components/breadcrumb';
-import { CreateOrder } from '@/components/forms/task-stepper/create-task';
 import { CreateProductForm } from '@/components/forms/product-stepper/create-product';
-import { CreateSubscriptionOne } from '@/components/forms/subscription-stepper/create-subscription';
 import MainLayout from '@/components/layout/main-layout';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
